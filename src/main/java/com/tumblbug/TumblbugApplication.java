c08cf1c165e5e27d72d84942215205a018713e16
wjy5446 <whaledev.yoon@gmail.com>
2021-10-17 15:24:21 +0900

    스프링 초기화

---
+++ src/main/java/com/tumblbug/TumblbugApplication.java
@@ -0,0 +1,13 @@
+package com.tumblbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumblbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TumblbugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tumblbug/TumblbugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tumblbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TumblbugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
